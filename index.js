19b96a1b318cdcf89cfc197081677db38cf65288
Nifilia <anderokore@gmail.com>
2022-02-08 09:45:04 +0200

    Part 2

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const hbs =require('express-handlebars');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+	extname: 'hbs',
+	defaultLayout: 'main',
+	layoutsDir: __dirname + '/views/layouts/',
+}))
+
+app.use(express.static('public'));
+
 const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -15,6 +27,18 @@ var con = mysql.createConnection({
 con.connect(function(err){
 	if(err) throw err;
 	console.log("Connected to joga_mysql db");
+})
+
+app.get('/', (req, res) =>{
+	let query = "SELECT * FROM article";
+	let articles = []
+	con.query(query, (err, result) =>{
+		if(err) throw(err);
+		articles = result
+		res.render('index', {
+			articles: articles
+		})
+	})
 })
 
 app.listen(3000, () =>{
